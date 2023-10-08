a8f507c9cb72e70c5ffd4a8663c1310272b01405
gary <github@accu.ca>
2023-10-07 21:33:35 -0700

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,21 @@
 import {useState} from 'react';
-import useData from './hooks/useData';
-import DataList from './components/Dataist';
-import Status from './components/Status';
 import './App.css';
+import useData from './hooks/useData';
+import Status from './components/Status';
+import DataList from './components/Dataist';
 
 export default function App() {
   const [text, setText] = useState("");
   const {status, error, data, addItem, deleteItem} = useData();
 
-  const addFriend = function() {
-    addItem(text);
-  };
 
   return (
     <div className="App">
-      <h1>My Anything List</h1>
-
+      <h1>Hello React Final</h1>
       <Status status={status} error={error} />
 
-      <input type="text" value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={addFriend}>Add</button>
+      <input value={text} onChange={e => setText(e.target.value)} ></input>
+      <button onClick={() => addItem(text)}>Add</button>
 
       <DataList friends={data} deleteItem={deleteItem} />
     </div>
--- server/src/server.js
+++ server/src/server.js
@@ -7,12 +7,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// serve static files from ../build (needed for React)
 const cwd = process.cwd();
-const public = path.join(cwd, '..', 'public');
-console.log("public dir: ", public);
-app.use(express.static(public));
-// Note: Do Not make a route for "/" below or it will override our static public
+const public = path.join(cwd, "..", "public")
+app.use("/", express.static(public));
 
 // So we can read JSON body requests
 app.use(express.json());
@@ -27,7 +24,7 @@ let data = [
 ];
 
 app.get("/api/status", (req, res) => {
-  res.json({version: "1.01"});
+  res.json({version: "1.03"});
 });
 
 app.get("/api/data", (req, res) => {
