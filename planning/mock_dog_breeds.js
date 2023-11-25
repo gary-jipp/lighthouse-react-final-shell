851751c0c64ba4786e003d891b66890e88070083
Gregory Dechant <dechantg@gmail.com>
2023-11-24 20:46:07 -0800

    added mock dog data for testing

---
+++ planning/mock_dog_breeds.js
@@ -0,0 +1,20 @@
+
+
+
+INSERT INTO dog_breeds (dogId, breed_name) VALUES 
+(1,"Bearded Collie"),
+(2,"Border Collie"),
+(3,"Collie"),
+(4,"Golden Retriever"),
+(5,"American Bulldog"),
+(6,"Bulldog"),
+(7,"French Bulldog"),
+(8,"American Hairless Terrier"),
+(9,"American Staffordshire Terrier"),
+(10,"Boston Terrier"),
+(11,"Bull Terrier"),
+(12,"Irish Terrier"),
+(13,"Cavalier King Charles Spaniel"),
+(14,"Cocker Spaniel"),
+(15,"English Springer Spaniel");
+
