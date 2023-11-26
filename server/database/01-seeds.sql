a760fecb4d74ee16b815d8d53461a7371281e35e
Prabh <sonukaur68@gmail.com>
2023-11-25 19:03:11 -0700

    create database schema and seed data

--- server/database/01-seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO items (name) VALUES
-('Item 1'),
-('Item 2'),
-('Item 3'),
-('Item 4'),
-;
---
+++ server/database/recipes.sql
@@ -0,0 +1,5 @@
+INSERT INTO Recipes (name, description, instructions, nutritional_information, image_url, category_id, user_id)
+VALUES
+  ('Spaghetti Bolognese', 'Classic Italian pasta dish with meat sauce', 'Boil spaghetti. Cook ground beef with tomatoes and spices. Combine.', '{"calories": 600, "protein": 25, "carbs": 80}', 'images/spaghetti_bolognese.jpg', 1, 1);
+ 
+ 
