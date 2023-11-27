9cbe4dd775009703d16e3420ed9bc0d89a38fcec
Gregory Dechant <dechantg@gmail.com>
2023-11-27 12:49:55 -0800

    tested the gpt4 variation

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 async function main() {
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: 'user', content: 'Tell me about american bulldogs' }],
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4',
   });
   console.log(chatCompletion);
   console.log(chatCompletion.choices[0].message.content);
