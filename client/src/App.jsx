95b8bbc7ce1508382a354c7de382c703611824a4
Gary J <github@accu.ca>
2024-06-09 21:26:42 -0700

    Merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,4 @@
 import {useState} from 'react';
-<<<<<<< HEAD
-=======
-import DataList from './components/DataList';
-import Status from './components/Status';
-import useApplicationData from './hooks/useApplicationData';
->>>>>>> 34ca8694fda63a1e4a4d78cae2b957e09f934de2
 import './App.css';
 import useData from './hooks/useData';
 import Status from './components/Status';
